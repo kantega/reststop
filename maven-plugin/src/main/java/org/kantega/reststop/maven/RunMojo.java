8e872e857c90500424b41c122db67b7f26bbd227
Eirik Bjørsnøs <eirbjo@gmail.com>
2013-09-19 12:01:49 +0200

    StartMojo now deploys plugin without development, RunMojo waits with server.join()

---
+++ maven-plugin/src/main/java/org/kantega/reststop/maven/RunMojo.java
@@ -0,0 +1,36 @@
+package org.kantega.reststop.maven;
+
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.eclipse.jetty.server.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+@Mojo(name = "run",
+        defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST,
+        requiresDirectInvocation = true,
+        requiresDependencyResolution = ResolutionScope.TEST)
+public class RunMojo extends AbstractReststopMojo {
+
+    @Override
+    protected void afterServerStart(Server server) throws MojoFailureException {
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            throw new MojoFailureException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected List<Plugin> getPlugins() {
+        ArrayList<Plugin> plugins = new ArrayList<>(super.getPlugins());
+        plugins.add(new Plugin(mavenProject.getGroupId(), "reststop-development-plugin", mavenProject.getVersion()));
+        return plugins;
+    }
+}
