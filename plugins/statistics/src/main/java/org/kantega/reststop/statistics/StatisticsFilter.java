b58370068487147161a9f77f752fa355f1c6e255
helaar <haakon1>
2013-09-24 15:00:46 +0200

    Added a logback-aware statistics plugin - reporting a simplified access-log

---
+++ plugins/statistics/src/main/java/org/kantega/reststop/statistics/StatisticsFilter.java
@@ -0,0 +1,49 @@
+package org.kantega.reststop.statistics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+
+/**
+
+ */
+public class StatisticsFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger("STATISTICS");
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        long before = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        StringBuilder b = new StringBuilder();
+        b.append(req.getRequestURI());
+
+        String auth = req.getHeader("Authorization");
+
+        if( auth != null){
+            final String[] usernameAndPassword = new String(DatatypeConverter.parseBase64Binary(auth.substring("Basic ".length())), "utf-8").split(":");
+            b.append("[").append(usernameAndPassword[0]).append("]");
+        }
+
+        b.append(" ").append(resp.getStatus()).append(" ").append(String.valueOf(System.currentTimeMillis() - before));
+
+        LOG.info( b.toString());
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ plugins/statistics/src/main/java/org/kantega/reststop/statistics/StatisticsPlugin.java
@@ -0,0 +1,15 @@
+package org.kantega.reststop.statistics;
+
+import org.kantega.reststop.api.DefaultReststopPlugin;
+import org.kantega.reststop.api.Reststop;
+
+/**
+
+ */
+public class StatisticsPlugin  extends DefaultReststopPlugin {
+
+    public StatisticsPlugin(Reststop reststop) {
+
+        addServletFilter(reststop.createFilter(new StatisticsFilter(),"/*"));
+    }
+}
