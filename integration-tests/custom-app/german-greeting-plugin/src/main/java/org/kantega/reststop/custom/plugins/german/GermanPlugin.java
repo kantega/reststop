c83584ed5107ca1bf381489d0649c9ed1488daab
Eirik Bjørsnøs <eirbjo@gmail.com>
2013-09-27 11:18:42 +0200

    Adding german plugin to custom app

---
+++ integration-tests/custom-app/german-greeting-plugin/src/main/java/org/kantega/reststop/custom/plugins/german/GermanPlugin.java
@@ -0,0 +1,13 @@
+package org.kantega.reststop.custom.plugins.german;
+
+import org.kantega.reststop.custom.api.DefaultCustomAppPlugin;
+
+/**
+ *
+ */
+public class GermanPlugin extends DefaultCustomAppPlugin  {
+
+    public GermanPlugin() {
+        addGreeting("Hallo Welt");
+    }
+}
