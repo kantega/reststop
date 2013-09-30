9c6b6f42a9096d2f492143cc7ef71522b5718121
Eirik Bjørsnøs <eirbjo@gmail.com>
2013-09-30 22:46:17 +0200

    Simplified Spring controller

--- integration-tests/hello-world/src/main/java/org/kantega/reststop/helloworld/springmvc/HelloController.java
+++ integration-tests/hello-world/src/main/java/org/kantega/reststop/helloworld/springmvc/HelloController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 /**
  *
@@ -15,8 +15,7 @@ import java.io.IOException;
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String hello(HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello, my world!");
-        return null;
+    public void hello(Writer out) throws IOException {
+        out.write("Hello World, it's Spring time!");
     }
 }
