cd5b09d1d18963d8138cf0e29f60e34fd2b943b5
Eirik Bjørsnøs <eirbjo@gmail.com>
2013-10-08 12:34:19 +0200

    Find correct template for @Path-annotated method

--- plugins/jaxrs-metrics/src/main/java/org/kantega/reststop/jerseymetrics/TimerFeature.java
+++ plugins/jaxrs-metrics/src/main/java/org/kantega/reststop/jerseymetrics/TimerFeature.java
@@ -3,6 +3,7 @@ package org.kantega.reststop.jerseymetrics;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.*;
 import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -15,9 +16,17 @@ public class TimerFeature implements DynamicFeature {
     @Override
     public void configure(ResourceInfo resourceInfo, FeatureContext context) {
 
-        Path path = resourceInfo.getResourceClass().getAnnotation(Path.class);
+        Path methodPath = resourceInfo.getResourceMethod().getAnnotation(Path.class);
+        Path classPath  = resourceInfo.getResourceClass().getAnnotation(Path.class);
+
+        Path path = methodPath != null ? methodPath : classPath;
         if(path != null) {
-            context.register(new TimerBeforeFilter(path.value()));
+            UriBuilder builder = methodPath != null
+                    ? UriBuilder.fromResource(resourceInfo.getResourceClass()).path(resourceInfo.getResourceClass(),resourceInfo.getResourceMethod().getName())
+                    : UriBuilder.fromResource(resourceInfo.getResourceClass());
+
+            String template = builder.toTemplate();
+            context.register(new TimerBeforeFilter(template));
             context.register(TimerAfterFilter.class);
         }
     }
