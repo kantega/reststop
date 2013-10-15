095105e34180e0687c868a9dc4c20d8ea997aea7
helaar <haakon1>
2013-10-15 22:08:37 +0200

    Added integration tests for implemented functionality

---
+++ integration-tests/hello-json/src/test/java/org/kantega/reststop/hellojson/HelloJSonIT.java
@@ -0,0 +1,64 @@
+package org.kantega.reststop.hellojson;
+/*
+ * Copyright 2013 Kantega AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HelloJSonIT {
+
+    @Test
+    public void testGetSeries() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + System.getProperty("reststopPort") + "/mySeries").openConnection();
+        connection.setRequestProperty("Authorization", "Basic " + DatatypeConverter.printBase64Binary("joe:joe".getBytes("utf-8")));
+        connection.setRequestProperty("Accept", "application/json");
+        String message = IOUtils.toString(connection.getInputStream());
+
+
+        assertThat(message, is("[{\"key\":\"demo-series\",\"id\":\"e081b2455d5a4edcb528c3cdb90b2161\"}]"));
+
+    }
+
+    @Test
+    public void testGetSeriesById() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + System.getProperty("reststopPort") + "/mySeries/e081b2455d5a4edcb528c3cdb90b2161").openConnection();
+        connection.setRequestProperty("Authorization", "Basic " + DatatypeConverter.printBase64Binary("joe:joe".getBytes("utf-8")));
+        connection.setRequestProperty("Accept", "application/json");
+        String message = IOUtils.toString(connection.getInputStream());
+
+
+        assertThat(message, is("{\"key\":\"demo-series\",\"id\":\"e081b2455d5a4edcb528c3cdb90b2161\"}"));
+
+    }
+
+    @Test
+    public void testGetSeriesById404() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + System.getProperty("reststopPort") + "/mySeries/e081b2455d5a4edcb528c3cdb90b2162").openConnection();
+        connection.setRequestProperty("Authorization", "Basic " + DatatypeConverter.printBase64Binary("joe:joe".getBytes("utf-8")));
+        connection.setRequestProperty("Accept", "application/json");
+        assertThat(connection.getResponseCode(),is(404));
+    }
+
+
+}
