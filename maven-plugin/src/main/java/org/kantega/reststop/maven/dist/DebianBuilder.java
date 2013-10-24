6dfd82b05a717d47b5dbff85a36ee553fa568424
Helge Olav Aarstein <helge.olav.aarstein@kantega.no>
2013-10-24 15:03:24 +0200

    Started creating a builder for Debian packages

--- maven-plugin/src/main/java/org/kantega/reststop/maven/dist/DebianBuilder.java
+++ maven-plugin/src/main/java/org/kantega/reststop/maven/dist/DebianBuilder.java
@@ -1,11 +1,38 @@
 package org.kantega.reststop.maven.dist;
 
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+import java.io.*;
+
 /**
 
  */
 public class DebianBuilder {
 
-    private void writeConfFile() {
+    private final MavenProject mavenProject;
+
+    public DebianBuilder(MavenProject mavenProject) {
+        this.mavenProject = mavenProject;
+    }
+
+    private void writeConfFile(File spec) throws MojoExecutionException {
+        try (PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(spec)))) {
+
+            pw.println("Package: [[name]]");
+            pw.println("Version: [[version]]");
+            pw.println("Section: Java");
+            pw.println("Priority: optional"); //todo
+            pw.println("Architecture: all");
+            pw.println("Maintainer: ");  // todo
+            pw.println("Description: " + mavenProject.getDescription());
+            pw.println("Distribution: development");  // todo
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
 
     }
 }
