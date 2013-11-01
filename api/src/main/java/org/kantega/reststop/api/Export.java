916e4fc261306858898d5494b4272ad86fc5e71f
Eirik Bjørsnøs <eirbjo@gmail.com>
2013-11-01 09:36:52 +0100

    Add missing Export annotation

---
+++ api/src/main/java/org/kantega/reststop/api/Export.java
@@ -0,0 +1,15 @@
+package org.kantega.reststop.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Export {
+
+}
