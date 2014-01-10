0ebd84460e5810f0fa61da193b45f376103811cb
Eirik Bjørsnøs <eirbjo@gmail.com>
2014-01-10 16:41:09 +0100

    Missing file

---
+++ plugins/development/src/main/java/org/kantega/reststop/development/PluginStartupException.java
@@ -0,0 +1,10 @@
+package org.kantega.reststop.development;
+
+/**
+ *
+ */
+public class PluginStartupException extends RuntimeException {
+    public PluginStartupException(Exception e) {
+        super(e);
+    }
+}
