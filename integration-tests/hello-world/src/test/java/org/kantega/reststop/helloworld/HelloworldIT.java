66db14498ef71914369361a1a19ef97d4de7cc09
Eirik Bjørsnøs <eirbjo@gmail.com>
2014-02-03 13:52:23 +0100

    Enable bean validation

--- integration-tests/hello-world/src/test/java/org/kantega/reststop/helloworld/HelloworldIT.java
+++ integration-tests/hello-world/src/test/java/org/kantega/reststop/helloworld/HelloworldIT.java
@@ -40,7 +40,7 @@ public class HelloworldIT {
     public void shouldReturnHelloWorld() throws IOException {
 
 
-        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + System.getProperty("reststopPort") + "/helloworld/en").openConnection();
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:" + System.getProperty("reststopPort") + "/helloworld/en?yo=hello").openConnection();
         connection.setRequestProperty("Authorization", "Basic " + DatatypeConverter.printBase64Binary("joe:joe".getBytes("utf-8")));
         connection.setRequestProperty("Accept", "application/json");
         String message = IOUtils.toString(connection.getInputStream());
