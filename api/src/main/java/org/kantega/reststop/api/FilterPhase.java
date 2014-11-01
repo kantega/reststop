5da36b780160eb992f3343211415edd5b29c8815
Eirik Bjørsnøs <eirbjo@gmail.com>
2014-11-01 20:22:47 +0100

    Add more flexible FIlterPhases

--- api/src/main/java/org/kantega/reststop/api/FilterPhase.java
+++ api/src/main/java/org/kantega/reststop/api/FilterPhase.java
@@ -4,8 +4,16 @@ package org.kantega.reststop.api;
  *
  */
 public enum FilterPhase {
+    PRE_UNMARSHAL,
     UNMARSHAL,
+    POST_UNMARSHAL,
+    PRE_AUTHENTICATION,
     AUTHENTICATION,
+    POST_AUTHENTICATION,
+    PRE_AUTHORIZATION,
     AUTHORIZATION,
-    USER
+    POST_AUTHORIZATION,
+    PRE_USER,
+    USER,
+    POST_USER
 }
