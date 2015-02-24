b76c55d9879ebbf6d58f882d935665166eabd6bf
Espen Fossen <espenaf@junta.no>
2015-02-24 11:14:32 +0100

    Changed webjars plugin to return newest version of each artifactId.

--- plugins/webjars/src/main/java/org/kantega/reststop/webjars/WebjarsReststopPlugin.java
+++ plugins/webjars/src/main/java/org/kantega/reststop/webjars/WebjarsReststopPlugin.java
@@ -79,7 +79,16 @@ public class WebjarsReststopPlugin extends DefaultReststopPlugin {
                     version = version.substring(0, version.lastIndexOf("-"));
                 }
 
-                versions.put("versions." + artifactId, version);
+                String key = "versions." + artifactId;
+                if (versions.get(key) != null) {
+                    Version a = new Version(version);
+                    Version b = new Version(versions.get(key));
+                    if (a.compareTo(b) == 1) {
+                        versions.put(key, version);
+                    }
+                } else {
+                    versions.put(key, version);
+                }
             }
 
             return versions;
@@ -87,4 +96,53 @@ public class WebjarsReststopPlugin extends DefaultReststopPlugin {
             return versions;
         }
     }
+}
+
+class Version implements Comparable<Version> {
+
+    private String version;
+
+    public final String get() {
+        return this.version;
+    }
+
+    public Version(String version) {
+        if (version == null)
+            throw new IllegalArgumentException("Version can not be null");
+        if (!version.matches("^[0-9]+(\\.[0-9]+)*$"))
+            throw new IllegalArgumentException("Invalid version format");
+        this.version = version;
+    }
+
+    @Override
+    public int compareTo(Version that) {
+        if (that == null)
+            return 1;
+        String[] thisParts = this.get().split("\\.");
+        String[] thatParts = that.get().split("\\.");
+        int length = Math.max(thisParts.length, thatParts.length);
+        for (int i = 0; i < length; i++) {
+            int thisPart = i < thisParts.length ?
+                    Integer.parseInt(thisParts[i]) : 0;
+            int thatPart = i < thatParts.length ?
+                    Integer.parseInt(thatParts[i]) : 0;
+            if (thisPart < thatPart)
+                return -1;
+            if (thisPart > thatPart)
+                return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that)
+            return true;
+        if (that == null)
+            return false;
+        if (this.getClass() != that.getClass())
+            return false;
+        return this.compareTo((Version) that) == 0;
+    }
+
 }
