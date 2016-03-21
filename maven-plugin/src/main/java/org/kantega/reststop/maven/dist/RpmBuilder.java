28ec8329484ed828828cd0e6dd444a3038ee1227
Helge Olav Aarstein <helge.olav.aarstein@kantega.no>
2016-03-21 12:58:40 +0100

    Using chkconfig --add on service install

--- maven-plugin/src/main/java/org/kantega/reststop/maven/dist/RpmBuilder.java
+++ maven-plugin/src/main/java/org/kantega/reststop/maven/dist/RpmBuilder.java
@@ -153,6 +153,7 @@ public class RpmBuilder extends AbstractDistMojo {
 
             pw.println("Requires(pre): /usr/sbin/useradd, /usr/bin/getent");
             pw.println("%pre");
+            pw.println("/sbin/chkconfig --add %{name} 2> /dev/null || :");
             addAppuser(pw);
 
             pw.println("%preun");
