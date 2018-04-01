89e6cee8a2a5b3912bae00228f74a1b94b71fd94
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2018-04-01 13:33:38 +0200

    Junit 5 + an web socket integration test junit-platform-surefire-provider depends on a surefire-api 2.19, so need to wait for next release of junit to get things running

--- integration-tests/hello-world/src/test/java/org/kantega/reststop/helloworld/HelloTest.java
+++ integration-tests/hello-world/src/test/java/org/kantega/reststop/helloworld/HelloTest.java
@@ -19,9 +19,7 @@ package org.kantega.reststop.helloworld;
 import org.junit.Test;
 import org.kantega.reststop.helloworld.jaxrs.Hello;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 /**
  *
