474233cf9dc95ed2c3635dff51469f9098689995
Jon Kjennbakken <jon.kjennbakken@kantega.no>
2018-10-25 14:38:38 +0200

    Updated some dependencies with known vulnerabilities

--- maven-plugin/src/main/java/org/kantega/reststop/maven/RedeployableServerContainer.java
+++ maven-plugin/src/main/java/org/kantega/reststop/maven/RedeployableServerContainer.java
@@ -16,34 +16,30 @@
 
 package org.kantega.reststop.maven;
 
-import org.eclipse.jetty.http.pathmap.MappedResource;
 import org.eclipse.jetty.websocket.jsr356.server.ServerContainer;
 import org.eclipse.jetty.websocket.jsr356.server.ServerEndpointMetadata;
-import org.eclipse.jetty.websocket.server.MappedWebSocketCreator;
-import org.eclipse.jetty.websocket.server.WebSocketServerFactory;
-import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
+import org.eclipse.jetty.websocket.server.NativeWebSocketConfiguration;
 
 import javax.websocket.DeploymentException;
 import javax.websocket.server.ServerEndpointConfig;
-import java.util.Iterator;
 import java.util.concurrent.Executor;
 
 /**
  *
  */
 public class RedeployableServerContainer extends ServerContainer {
-    private final MappedWebSocketCreator creator;
+    private final NativeWebSocketConfiguration configuration;
 
-    public RedeployableServerContainer(MappedWebSocketCreator creator, WebSocketServerFactory factory, Executor executor) {
-        super(creator, factory, executor);
-        this.creator = creator;
+    public RedeployableServerContainer(NativeWebSocketConfiguration configuration, Executor executor) {
+        super(configuration, executor);
+        this.configuration = configuration;
     }
 
     @Override
     public void addEndpoint(Class<?> endpointClass) throws DeploymentException {
         if (isStarted() || isStarting()) {
             ServerEndpointMetadata metadata = getServerEndpointMetadata(endpointClass, null);
-            removeMapping(metadata.getPath());
+            configuration.removeMapping(metadata.getPath());
             super.addEndpoint(endpointClass);
         } else {
             super.addEndpoint(endpointClass);
@@ -54,20 +50,11 @@ public class RedeployableServerContainer extends ServerContainer {
     @Override
     public void addEndpoint(ServerEndpointConfig config) throws DeploymentException {
         if (isStarted() || isStarting()) {
-            removeMapping(config.getPath());
+            configuration.removeMapping(config.getPath());
             super.addEndpoint(config);
         } else {
             super.addEndpoint(config);
         }
     }
-
-    private void removeMapping(String path) {
-        Iterator<MappedResource<WebSocketCreator>> iterator = creator.getMappings().iterator();
-        while (iterator.hasNext()) {
-            MappedResource<WebSocketCreator> next = iterator.next();
-            if(next.getPathSpec().getDeclaration().equals(path)) {
-                iterator.remove();
-            }
-        }
-    }
 }
+
